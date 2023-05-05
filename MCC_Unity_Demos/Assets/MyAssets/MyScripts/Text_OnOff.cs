1714ea3b8a159cab10eb6a184a0af0f3e5560e4d
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-05-05 00:06:52 -0400

    Just Testing

---
+++ MCC_Unity_Demos/Assets/MyAssets/MyScripts/Text_OnOff.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text_OnOff : MonoBehaviour
+{
+    public GameObject textOBJ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textOBJ.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        textOBJ.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        textOBJ.SetActive(false);
+    }
+}
